630e8b8c1eead056c70971534021c02b6032ac25
johnalexiv <johnalexiv@gmail.com>
2017-11-27 20:50:58 -0800

    Added result discussion at end

--- project6a.c
+++ project6a.c
@@ -199,8 +199,43 @@ void waitForWriters(struct Writer *writers)
         pthread_join(writers[i].thread, NULL);
 }
 
+/*
+Most of the time writers will write before readers are initialized and can get into
+the critical section to read the variable. At times when readers do get in, multiple 
+readers will often get into the critical section to read the variable
+before any writers can get in. I am able to produce these results by inserting
+usleep times into the writer thread function and reader thread function. Where
+the usleep in the writer function is before the writer can access the critical
+section, and the reader usleep is inserted after locking the shared variable to
+allow multiple readers into the critical section to read the shard variable at 
+the same time.
 
-
+sample output:
+Writer 1 is entering the database
+Writer 1 updade value to 1
+Writer 1 is leaving database
+Reader 0 is entering the database
+Writer 0 is entering the database
+Writer 0 updade value to 2
+Writer 0 is leaving database
+Writer 2 is entering the database
+Writer 2 updade value to 3
+Reader 2 is entering the database
+Reader 0 read value from database 3
+Reader 0 is leaving database
+Writer 2 is leaving database
+Reader 1 is entering the database
+Reader 2 read value from database 3
+Reader 2 is leaving database
+Reader 1 read value from database 3
+Reader 1 is leaving database
+Reader 3 is entering the database
+Writer 3 is entering the database
+Writer 3 updade value to 4
+Writer 3 is leaving database
+Reader 3 read value from database 4
+Reader 3 is leaving database
+*/
 
 
 
